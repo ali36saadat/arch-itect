9bd69ca861558d3daecbd7a6cf7aa7f5aa418411
Ali Saadat <ali36saadat@gmail.com>
2026-09-17 18:22:17 +0330

    Add extra_mirrors

--- 00.init.d/03.mirrors.sh
+++ 00.init.d/03.mirrors.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 . "$(dirname "$BASH_SOURCE")/../utils.sh"
 
+extra_mirrors=(
+    "https://linux-mirror.liara.ir/repository/arch/"
+)
+
 read -rn 1 -p "Update Mirrors? [y/N] " umirrors
 echo
 
@@ -8,6 +12,10 @@ if [[ $umirrors =~ ^([Yy])$ ]]; then
   title "Update Mirrors"
   sudo pacman -Sy reflector --noconfirm
   sudo reflector --sort rate --latest 10 --save /etc/pacman.d/mirrorlist
+
+  for mirror in "${extra_mirrors[@]}"; do
+    echo "Server = ${mirror}\$repo/os/\$arch"
+  done | sudo tee -a /etc/pacman.d/mirrorlist >/dev/null
 fi
 
 read -rn 1 -p "Sync Mirrors? [y/N] " sync
